3d43a33f5d308c5498145338fed39d1f87d3ca18
KANIYAMUDHAN Y <112994943+kaniyamudhan@users.noreply.github.com>
2023-09-07 19:44:22 +0530

    Update main.php

--- main.php
+++ main.php
@@ -2,9 +2,6 @@
 
 include("config.php");
 include("session.php");
-
-//count total exp
-
 $v=0;
 $v2=0;
 $de="Not availabe";
@@ -25,8 +22,6 @@ $tt=0;
 	$v= $years . " Years, " . $months . " Months, " . $days . " Days";
 
 	}
-//count mkce exp
-
 	$query = "select ID, sum(datediff(now() , PASS) +1 )AS value_sum from login group by ID having ID='$s';";
 	$query_run = mysqli_query($db, $query);
 	if(mysqli_num_rows($query_run) > 0){
@@ -35,12 +30,9 @@ $tt=0;
 	$years = floor($sum / 365);
 	$months = floor(($sum - ($years * 365))/30.5);
 	$days = floor($sum - ($years * 365) - ($months * 30.5));
-							//echo "Days received: " . $sum . " days <br />";
+						
 	$v2= $years . " Years, " . $months . " Months, " . $days . " Days";
 	}
-
-//active days
-
 	$query = "SELECT ID,PASS FROM login WHERE ID='$s'";
     $query_run = mysqli_query($db, $query);
 	if(mysqli_num_rows($query_run) > 0){
@@ -65,59 +57,31 @@ $tt=0;
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon.png">
     <title>SRMS</title>
-    <!-- Custom CSS -->
+  
     <link href="assets/libs/flot/css/float-chart.css" rel="stylesheet">
-    <!-- Custom CSS -->
     <link href="dist/css/style.min.css" rel="stylesheet">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-<![endif]-->
 </head>
 
 <body>
-    <!-- ============================================================== -->
-    <!-- Preloader - style you can find in spinners.css -->
-    <!-- ============================================================== -->
     <div class="preloader">
         <div class="lds-ripple">
             <div class="lds-pos"></div>
             <div class="lds-pos"></div>
         </div>
     </div>
-    <!-- ============================================================== -->
-    <!-- Main wrapper - style you can find in pages.scss -->
-    <!-- ============================================================== -->
     <div id="main-wrapper">
-        <!-- ============================================================== -->
-        <!-- Topbar header - style you can find in pages.scss -->
-        <!-- ============================================================== -->
         <header class="topbar" data-navbarbg="skin5">
             <nav class="navbar top-navbar navbar-expand-md navbar-dark">
                 <div class="navbar-header" data-logobg="skin5">
-                    <!-- This is for the sidebar toggle which is visible on mobile only -->
                     <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i class="ti-menu ti-close"></i></a>
-                    <!-- ============================================================== -->
-                    <!-- Logo -->
-                    <!-- ============================================================== -->
                     <a class="navbar-brand" href="main">
-                        <!-- Logo icon -->
-     
-                        <!--End Logo icon -->
-                         <!-- Logo text -->
                         <span class="logo-text">
-                             <!-- dark Logo text -->
                              <img src="assets/images/srms.png" alt="homepage" class="light-logo" />
-                            
                         </span>
                     <a class="topbartoggler d-block d-md-none waves-effect waves-light" href="javascript:void(0)" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i class="ti-more"></i></a>
                 </div>
@@ -125,28 +89,7 @@ $tt=0;
                     <ul class="navbar-nav float-left mr-auto">
                         <li class="nav-item d-none d-md-block"><a class="nav-link sidebartoggler waves-effect waves-light" href="javascript:void(0)" data-sidebartype="mini-sidebar"><i class="mdi mdi-menu font-24"></i></a></li>
                     </ul>
-                    <!-- ============================================================== -->
-                    <!-- Right side toggle and nav items -->
-                    <!-- ============================================================== -->
                     <ul class="navbar-nav float-right">
-                        <!-- ============================================================== -->
-                        <!-- Comment -->
-                        <!-- ============================================================== -->
-
-                        <!-- ============================================================== -->
-                        <!-- End Comment -->
-                        <!-- ============================================================== -->
-                        <!-- ============================================================== -->
-                        <!-- Messages -->
-                        <!-- ============================================================== -->
-      
-                        <!-- ============================================================== -->
-                        <!-- End Messages -->
-                        <!-- ============================================================== -->
-
-                        <!-- ============================================================== -->
-                        <!-- User profile and search -->
-                        <!-- ============================================================== -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark pro-pic" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="assets/images/users/1.jpg" alt="user" class="rounded-circle" width="31"></a>
                             <div class="dropdown-menu dropdown-menu-right user-dd animated">
@@ -155,30 +98,12 @@ $tt=0;
                                 <div class="dropdown-divider"></div>
                             </div>
                         </li>
-                        <!-- ============================================================== -->
-                        <!-- User profile and search -->
-                        <!-- ============================================================== -->
                     </ul>
                 </div>
             </nav>
         </header>
-        <!-- ============================================================== -->
-        <!-- End Topbar header -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Left Sidebar - style you can find in sidebar.scss  -->
-        <!-- ============================================================== -->
 			<?php include("Aside.php"); ?>
-        <!-- ============================================================== -->
-        <!-- End Left Sidebar - style you can find in sidebar.scss  -->
-        <!-- ============================================================== -->
-        <!-- ============================================================== -->
-        <!-- Page wrapper  -->
-        <!-- ============================================================== -->
         <div class="page-wrapper">
-            <!-- ============================================================== -->
-            <!-- Bread crumb and right sidebar toggle -->
-            <!-- ============================================================== -->
              <div class="page-breadcrumb">
                 <div class="row">
                     <div class="col-12 d-flex no-block align-items-center">
@@ -194,30 +119,21 @@ $tt=0;
                     </div>
                 </div>
             </div>
-            <!-- ============================================================== -->
-            <!-- End Bread crumb and right sidebar toggle -->
-            <!-- ============================================================== -->
-            <!-- ============================================================== -->
-            <!-- Container fluid  -->
-            <!-- ============================================================== -->
+          
             <div class="container-fluid">
-                <!-- ============================================================== -->
-                <!-- Sales Cards  -->
-                <!-- ============================================================== -->
+               
                 <div class="row">
-                    <!-- Column -->
+                 
                     <div class="col-md-6 col-lg-4 col-xlg-6">
                         <div class="card card-hover">
                             <div class="box bg-cyan text-center">
                                 <h1 class="font-light text-white"><i class="fa fa-user"></i></h1>
                                 <h4 class="text-white"><b>Total Experience</b></h4>
 								<h4 class="text-white"><b><?php echo $v; ?> </b></h4>
-								
-
                             </div>
                         </div>
                     </div>
-                    <!-- Column -->
+                   
                     <div class="col-md-6 col-lg-4 col-xlg-3">
                         <div class="card card-hover">
                             <div class="box bg-success text-center">
@@ -227,7 +143,7 @@ $tt=0;
                             </div>
                         </div>
                     </div>
-                     <!-- Column -->
+     
                     <div class="col-md-6 col-lg-4 col-xlg-3">
                         <div class="card card-hover">
                             <div class="box bg-warning text-center">
@@ -237,7 +153,7 @@ $tt=0;
                             </div>
                         </div>
                     </div>
-                    <!-- Column -->
+
                     <div class="col-md-6 col-lg-4 col-xlg-3">
                         <div class="card card-hover">
                             <div class="box bg-danger text-center">
@@ -247,7 +163,7 @@ $tt=0;
                             </div>
                         </div>
                     </div>
-                    <!-- Column -->
+
                     <div class="col-md-6 col-lg-4 col-xlg-3">
                         <div class="card card-hover">
                             <div class="box bg-info text-center">
@@ -257,8 +173,7 @@ $tt=0;
                             </div>
                         </div>
                     </div>
-                    <!-- Column -->
-                    <!-- Column -->
+           
                     <div class="col-md-6 col-lg-4 col-xlg-3">
                         <div class="card card-hover">
                             <div class="box bg-secondary text-center">
@@ -272,7 +187,7 @@ $tt=0;
                 </div>
 								<?php
 									
-							//count basic		
+						
 									$query2 = "SELECT * FROM basic WHERE id='$s'";
 									$query_run2 = mysqli_query($db, $query2);
 
@@ -321,8 +236,7 @@ $tt=0;
 									$cf=$c1+$c2+$c3+$c;
 						
 
-						//count academic			
-									
+					
 									
 								$query2 = "SELECT * FROM exp WHERE id='$s'";
 									$query_run2 = mysqli_query($db, $query2);
@@ -372,13 +286,10 @@ $tt=0;
 									$querybc = "UPDATE faculty SET bc='$cf',ac='$cd' WHERE id='$s'";
 									$query_runbc = mysqli_query($db, $querybc);								
 										?>
-                <!-- ============================================================== -->
-                <!-- Recent comment and chats -->
-                <!-- ============================================================== -->
+             
                 <div class="row">
-                    <!-- column -->
                     <div class="col-lg-12">
-                        <!-- card -->
+  
                         <div class="card">
                             <div class="card-body">
                                 <h4 class="card-title m-b-0">Progress Box</h4>
@@ -416,46 +327,28 @@ $tt=0;
                         </div>
                     </div>
                 </div>
-                <!-- ============================================================== -->
-                <!-- Recent comment and chats -->
-                <!-- ============================================================== -->
+             
             </div>
-            <!-- ============================================================== -->
-            <!-- End Container fluid  -->
-            <!-- ============================================================== -->
-            <!-- ============================================================== -->
-            <!-- footer -->
-            <!-- ============================================================== -->
+         
             		 <?php include "./footer.html" ?>
-            <!-- ============================================================== -->
-            <!-- End footer -->
-            <!-- ============================================================== -->
+          
         </div>
-        <!-- ============================================================== -->
-        <!-- End Page wrapper  -->
-        <!-- ============================================================== -->
+    
     </div>
-    <!-- ============================================================== -->
-    <!-- End Wrapper -->
-    <!-- ============================================================== -->
-    <!-- ============================================================== -->
-    <!-- All Jquery -->
-    <!-- ============================================================== -->
+  
     <script src="assets/libs/jquery/dist/jquery.min.js"></script>
-    <!-- Bootstrap tether Core JavaScript -->
+ 
     <script src="assets/libs/popper.js/dist/umd/popper.min.js"></script>
     <script src="assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
     <script src="assets/extra-libs/sparkline/sparkline.js"></script>
-    <!--Wave Effects -->
+ 
     <script src="dist/js/waves.js"></script>
-    <!--Menu sidebar -->
+
     <script src="dist/js/sidebarmenu.js"></script>
-    <!--Custom JavaScript -->
+
     <script src="dist/js/custom.min.js"></script>
-    <!--This page JavaScript -->
-    <!-- <script src="dist/js/pages/dashboards/dashboard1.js"></script> -->
-    <!-- Charts js Files -->
+  
     <script src="assets/libs/flot/excanvas.js"></script>
     <script src="assets/libs/flot/jquery.flot.js"></script>
     <script src="assets/libs/flot/jquery.flot.pie.js"></script>
